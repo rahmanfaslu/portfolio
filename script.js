c77412b8952794d07c1eb913f381fb35ae8dc3f5
Faslu rahman <rahmanfaslu33@gmail.com>
2025-05-30 20:44:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function showSlideBar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
+}
+
+   function hideSlidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
+}
